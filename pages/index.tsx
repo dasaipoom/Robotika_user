1e1c67ce09774ff2930e0cacc35eeed7e08e979e
Poomrapee Chaiprsop <poomrapee.cps@gmail.com>
2023-07-07 17:01:01 +0700

    update connect page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="pt-40"></div>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 gap-20 lg:text-left">
         <Link
-          href="http://localhost:83/"
+          href="http://compreface-fe:80"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
